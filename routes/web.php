81cf47a49879ca5aae9268928faec8d11645794d
SoyAnderEscobar <soyanderescobar@gmail.com>
2022-04-11 22:41:18 -0400

    creacin de plantilla y primera vista del sistema

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+    // return view('welcome');
+})->name('home');
 
 Route::middleware([
     'auth:sanctum',
